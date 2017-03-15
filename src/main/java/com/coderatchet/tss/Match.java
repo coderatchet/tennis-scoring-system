30a524df7bc3004b576ad083fd6c933a196996a2
coderatchet <jarednagle@gmail.com>
2017-03-15 13:44:10 +1100

    added initialized structure of a match

--- src/main/java/com/coderatchet/tss/Match.java
+++ src/main/java/com/coderatchet/tss/Match.java
@@ -1,8 +1,58 @@
 package com.coderatchet.tss;
 
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
 /**
  * Created by jared on 14/03/17.
  */
 public class Match {
 
+    private String p1Name;
+    private String p2Name;
+    private int p1GamesWon = 0;
+    private int p2GamesWon = 0;
+    private int p1GameScore = 0;
+    private int p2GameScore = 0;
+    private int currentGame = 1;
+
+    public Match(String player1Name, String player2Name) {
+        p1Name = player1Name;
+        p2Name = player2Name;
+    }
+
+    public void pointWonBy(String playerName) {
+        throw new NotImplementedException();
+    }
+
+    public void score() {
+        throw new NotImplementedException();
+    }
+
+    public String getP1Name() {
+        return p1Name;
+    }
+
+    public String getP2Name() {
+        return p2Name;
+    }
+
+    public int getP1GamesWon() {
+        return p1GamesWon;
+    }
+
+    public int getP2GamesWon() {
+        return p2GamesWon;
+    }
+
+    public int getP1GameScore() {
+        return p1GameScore;
+    }
+
+    public int getP2GameScore() {
+        return p2GameScore;
+    }
+
+    public int getCurrentGame() {
+        return currentGame;
+    }
 }
--- src/test/java/com/coderatchet/tss/MatchTest.java
+++ src/test/java/com/coderatchet/tss/MatchTest.java
@@ -1,10 +1,37 @@
 package com.coderatchet.tss;
 
-import static org.junit.Assert.*;
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Created by jared on 15/03/17.
  */
 public class MatchTest {
 
+//    TODO test output of score system with method found from http://stackoverflow.com/questions/1119385/junit-test-for-system-out-println
+//    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+//    private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+//
+//    @Before
+//    public void setUpStreams(){
+//        System.setOut(new PrintStream(outContent));
+//        System.setErr(new PrintStream(errContent));
+//    }
+
+    @Test
+    public void testCanCreateAndInitMatch(){
+        Match match = new Match("bob", "judy");
+        assertEquals("bob", match.getP1Name());
+        assertEquals("judy", match.getP2Name());
+        assertArrayEquals(new int[]{0, 0, 0, 0, 1}, new int[]{
+            match.getP1GamesWon(),
+            match.getP2GamesWon(),
+            match.getP1GameScore(),
+            match.getP2GameScore(),
+            match.getCurrentGame()});
+    }
 }
