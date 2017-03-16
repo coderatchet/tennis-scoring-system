e087f830aaf9eae981346a43a5dc08ae4b976902
coderatchet <jarednagle@gmail.com>
2017-03-16 13:25:13 +1100

    extended main game for extra functionality demonstration

--- src/main/java/com/coderatchet/tss/Main.java
+++ src/main/java/com/coderatchet/tss/Main.java
@@ -3,6 +3,8 @@ package com.coderatchet.tss;
 /**
  * Test scenario as presented in github challenge.
  *
+ * run mvn test for passing tests and investigate the MatchTest.java file for how the game works.
+ *
  * Created by jared on 16/03/17.
  */
 public class Main {
@@ -35,5 +37,51 @@ public class Main {
         match.pointWonBy(P1);
         // this will return "1-0"
         match.score();
+
+        // Rafael flogs Roger for 5 games
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 4; j++) {
+                match.pointWonBy(P2);
+            }
+        }
+
+        match.score();
+
+        // Roger makes a comeback and comes close to set win
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 4; j++) {
+                match.pointWonBy(P1);
+            }
+        }
+
+        match.score();
+
+        // Rafael brings us to tie breaker
+        match.pointWonBy(P2);
+        match.pointWonBy(P2);
+        match.pointWonBy(P2);
+        match.pointWonBy(P2);
+
+        match.score();
+
+        match.pointWonBy(P1);
+        match.pointWonBy(P2);
+        match.pointWonBy(P1);
+        match.pointWonBy(P2);
+        match.pointWonBy(P1);
+        match.pointWonBy(P2);
+        match.pointWonBy(P1);
+        match.pointWonBy(P2);
+        match.pointWonBy(P1);
+        match.pointWonBy(P2);
+        match.pointWonBy(P1);
+        match.pointWonBy(P2);
+        match.pointWonBy(P1);
+
+        // close game!!
+        match.score();
+
+        // Roger WINS!
+        match.pointWonBy(P1);
     }
 }
